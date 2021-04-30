6890c5df84a6a09d0550c6bc448fbe2a1e55067c
JLTDigital <mrjltaft@gmail.com>
2021-04-30 19:21:31 +0100

    Update

--- frontend/src/views/ExchangesView.js
+++ frontend/src/views/ExchangesView.js
@@ -5,17 +5,20 @@ import { Row, Col } from 'react-bootstrap'
 
 
 const ExchangesView = () => {
-  return (
-    <>
-      <Hero heading='Exchanges' para='Best places to make your crypto purchases, trades, loans and build some crypto savings.' />
 
-      <Row className='mt-3'>
-        <Col className='p2' md={12}>
-          
-        </Col>
-      </Row>
-    </>
-  )
-}
+    return (
+      <>
+        <Hero heading='Exchanges' para='Best places to make your crypto purchases, trades, loans and build some crypto savings.' />
+  
+        <Row className='mt-3'>
+          <Col className='p2' md={12}>
+            
+          </Col>
+        </Row>
+      </>
+    )
+  }
+
+
 
 export default ExchangesView

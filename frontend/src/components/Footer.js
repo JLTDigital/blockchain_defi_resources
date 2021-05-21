ae1b388df59bfb4b56ac9e885f91f1acdf580c38
JLTDigital <mrjltaft@gmail.com>
2021-05-21 15:47:49 +0100

    Update

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Row, Col, Container } from 'react-bootstrap'
 import BlockchainDeFiImg from '../assets/blockchainDefi.png'
-import { BiLinkExternal, BiEnvelopeOpen } from 'react-icons/bi'
+import { BiEnvelopeOpen } from 'react-icons/bi'
 
 const Footer = () => {
   return (
@@ -14,11 +14,6 @@ const Footer = () => {
           </Col>
           <Col md={6} className='p-2 mt-2 text-center'>
             <small>Built by JLTDigital - Fullstack Developer</small>
-            <br/>
-            <small>Issues: <a href="https://github.com/JLTDigital/blockchain_defi_resources">
-              <BiLinkExternal color='#000' />
-            </a> 
-            </small>
             <br/>
             <small>Contact me: <a href="http://jlt.digital">
             <BiEnvelopeOpen color='#000' />

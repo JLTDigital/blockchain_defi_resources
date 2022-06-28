6607b1cbd965148d7dc70fcdb2aadcc3ca0caa1b
JLTDigital <mrjltaft@gmail.com>
2022-06-28 22:56:26 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express()
 
 app.use(cors())
 
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
@@ -36,10 +36,12 @@ app.use('/api', walletRoutes)
 
 const __dirname = path.resolve()
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
-  
-  app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+
+  app.get('/*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
 } else {
   app.get('/api', (req, res) => {
     res.send('API is active...')
@@ -51,4 +53,9 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} at port ${PORT}`.blue.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} at port ${PORT}`.blue.bold
+  )
+)

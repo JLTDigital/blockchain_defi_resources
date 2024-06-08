02d0bd32ee4965008e2557e35b2a8ea4408f8933
JLTDigital <mrjltaft@gmail.com>
2024-06-08 15:07:51 +0100

    Update

--- backend/db/database.js
+++ backend/db/database.js
@@ -2,15 +2,10 @@ import mongoose from 'mongoose'
 
 export const database = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true
-    })
-    console.log(`Mongo Database Connected: ${connect.connection.host}`.bgYellow);
+    const connect = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`Mongo Database Connected: ${connect.connection.host}`.bgYellow)
   } catch (error) {
     console.log(`Error: ${error.message}`.red.bold)
     process.exit(1)
   }
 }
-
